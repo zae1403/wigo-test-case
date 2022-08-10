1e389644e39920c46b096ef853f6bd6ed006e36f
Muhammad <muhammadzaaelanisap@gmail.com>
2022-08-10 22:15:28 +0700

    added extension method isEmail and method capitalizeFirstLetter

---
+++ lib/presentation/utils/extensions.dart
@@ -0,0 +1,11 @@
+extension StringX on String {
+  bool get isEmail {
+    return RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(this);
+  }
+
+  String capitalizeFirstLetter() {
+    return substring(0, 1).toUpperCase() + substring(1);
+  }
+}
