27aaad496fc60fe27c9002f0545e70bf311977ce
Muhammad <muhammadzaaelanisap@gmail.com>
2022-08-10 22:10:16 +0700

    added class color palette

---
+++ lib/presentation/resources/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color backgroundWhit = Color(0xFF1F1D2B);
+  static const Color backgroundBlack = Color(0xFF1F1D2B);
+  static const Color backgroundBlackVariant = Color(0xFF2B2937);
+
+  static const Color primaryColor = Color(0xFF6C5ECF);
+  static const Color secondaryColor = Color(0xFF38ABBE);
+  static const Color alertColor = Color(0xFFED6363);
+  static const Color primaryTextColor = Color(0xFFF1F0F2);
+  static const Color secondaryTextColor = Color(0xFF504F5E);
+  static const Color disabledColor = Color(0xFF999999);
+}
